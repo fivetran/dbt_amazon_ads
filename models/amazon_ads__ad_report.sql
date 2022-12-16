f8c9a5eca94b34eddd80e9b7857a4a649dd421f5
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2022-12-16 11:46:50 -0600

    update ad_report

--- models/amazon_ads__ad_report.sql
+++ models/amazon_ads__ad_report.sql
@@ -2,7 +2,7 @@
 
 with report as (
     select *
-    from {{ var('ad_group_level_report') }}
+    from {{ var('advertised_product_report') }}
 ), 
 
 {% if var('amazon_ads__portfolio_history_enabled', True) %}
@@ -25,6 +25,12 @@ ad_groups as (
     where is_most_recent_record = True
 ), 
 
+ads as (
+    select *
+    from {{ var('product_ad_history') }}
+    where is_most_recent_record = True
+),
+
 fields as (
     select
         report.date_day,
@@ -38,23 +44,29 @@ fields as (
         campaigns.campaign_id,
         ad_groups.ad_group_name,
         ad_groups.ad_group_id,
-        ad_groups.serving_status,
-        ad_groups.state,
-        ad_groups.default_bid,
+        ads.ad_id,
+        ads.serving_status,
+        ads.state,
         
-        report.campaign_bidding_strategy,
+        report.advertised_asin,
+        report.advertised_sku,
+        report.campaign_budget_amount,
+        report.campaign_budget_currency_code,
+        report.campaign_budget_type,
         report.cost,
         report.clicks,
         report.impressions
 
-        {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='amazon_ads__ad_group_stats_passthrough_metrics') }}
+        {{ fivetran_utils.persist_pass_through_columns(pass_through_variable='amazon_ads__advertised_product_passthrough_metrics') }}
 
     from report
-
+    
+    left join ads
+        on report.ad_id = ads.ad_id
     left join ad_groups
         on report.ad_group_id = ad_groups.ad_group_id
     left join campaigns
-        on ad_groups.campaign_id = campaigns.campaign_id
+        on report.campaign_id = campaigns.campaign_id
 
     {% if var('amazon_ads__portfolio_history_enabled', True) %}
         left join portfolios
