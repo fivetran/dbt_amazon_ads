deb33ff3b75b78070181ef166fe10df0109ebb35
Renee Li <renee.li@fivetran.com>
2024-10-02 19:19:17 -0400

    docs, model updates, add tests, add documentation

---
+++ integration_tests/tests/consistency/consistency_ad_report.sql
@@ -0,0 +1,45 @@
+{{ config(
+    tags="fivetran_validations",
+    enabled=var('fivetran_validation_tests_enabled', false)
+) }}
+
+with prod as (
+    select
+        ad_id,
+        sum(clicks) as clicks, 
+        sum(impressions) as impressions,
+        sum(cost) as cost
+    from {{ target.schema }}_amazon_ads_prod.amazon_ads__account_report
+    group by 1
+),
+
+dev as (
+    select
+        ad_id,
+        sum(clicks) as clicks, 
+        sum(impressions) as impressions,
+        sum(cost) as cost
+    from {{ target.schema }}_amazon_ads_dev.amazon_ads__account_report
+    group by 1
+),
+
+final as (
+    select 
+        prod.ad_id,
+        prod.clicks as prod_clicks,
+        dev.clicks as dev_clicks,
+        prod.impressions as prod_impressions,
+        dev.impressions as dev_impressions,
+        prod.cost as prod_cost,
+        dev.cost as dev_cost
+    from prod
+    full outer join dev 
+        on dev.ad_id = prod.ad_id
+)
+
+select *
+from final
+where
+    abs(prod_clicks - dev_clicks) >= .01
+    or abs(prod_impressions - dev_impressions) >= .01
+    or abs(prod_cost - dev_cost) >= .01
---
+++ integration_tests/tests/consistency/consistency_keyword_report.sql
@@ -0,0 +1,45 @@
+{{ config(
+    tags="fivetran_validations",
+    enabled=var('fivetran_validation_tests_enabled', false)
+) }}
+
+with prod as (
+    select
+        keyword_id,
+        sum(clicks) as clicks, 
+        sum(impressions) as impressions,
+        sum(cost) as cost
+    from {{ target.schema }}_amazon_ads_prod.amazon_ads__account_report
+    group by 1
+),
+
+dev as (
+    select
+        keyword_id,
+        sum(clicks) as clicks, 
+        sum(impressions) as impressions,
+        sum(cost) as cost
+    from {{ target.schema }}_amazon_ads_dev.amazon_ads__account_report
+    group by 1
+),
+
+final as (
+    select 
+        prod.keyword_id,
+        prod.clicks as prod_clicks,
+        dev.clicks as dev_clicks,
+        prod.impressions as prod_impressions,
+        dev.impressions as dev_impressions,
+        prod.cost as prod_cost,
+        dev.cost as dev_cost
+    from prod
+    full outer join dev 
+        on dev.keyword_id = prod.keyword_id
+)
+
+select *
+from final
+where
+    abs(prod_clicks - dev_clicks) >= .01
+    or abs(prod_impressions - dev_impressions) >= .01
+    or abs(prod_cost - dev_cost) >= .01
