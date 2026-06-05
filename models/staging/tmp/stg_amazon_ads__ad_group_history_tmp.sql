047a780a6c40779d68383ac95b0455df4025a2d7
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2026-06-05 13:20:58 -0500

    tmp union update

--- models/staging/tmp/stg_amazon_ads__ad_group_history_tmp.sql
+++ models/staging/tmp/stg_amazon_ads__ad_group_history_tmp.sql
@@ -1,16 +1,6 @@
 {{ config(enabled=var('ad_reporting__amazon_ads_enabled', True)) }}
 
-{% if var('amazon_ads_sources',[]) != [] %}
-
-{{
-    fivetran_utils.union_connections(
-        connection_dictionary='amazon_ads_sources',
-        single_source_name='amazon_ads',
-        single_table_name='ad_group_history'
-    )
-}}
-
-{% else %}
+{% if var('amazon_ads_union_schemas', []) | length > 0 or var('amazon_ads_union_databases', []) | length > 0 %}
 
 {{
     fivetran_utils.union_data(
@@ -22,6 +12,16 @@
         default_variable='ad_group_history',
         union_schema_variable='amazon_ads_union_schemas',
         union_database_variable='amazon_ads_union_databases'
+    )
+}}
+
+{% else %}
+
+{{
+    fivetran_utils.union_connections(
+        connection_dictionary='amazon_ads_sources',
+        single_source_name='amazon_ads',
+        single_table_name='ad_group_history'
     )
 }}
 
--- models/staging/tmp/stg_amazon_ads__portfolio_history_tmp.sql
+++ models/staging/tmp/stg_amazon_ads__portfolio_history_tmp.sql
@@ -1,16 +1,6 @@
 {{ config(enabled=fivetran_utils.enabled_vars(['ad_reporting__amazon_ads_enabled','amazon_ads__portfolio_history_enabled'])) }}
 
-{% if var('amazon_ads_sources',[]) != [] %}
-
-{{
-    fivetran_utils.union_connections(
-        connection_dictionary='amazon_ads_sources',
-        single_source_name='amazon_ads',
-        single_table_name='portfolio_history'
-    )
-}}
-
-{% else %}
+{% if var('amazon_ads_union_schemas', []) | length > 0 or var('amazon_ads_union_databases', []) | length > 0 %}
 
 {{
     fivetran_utils.union_data(
@@ -22,6 +12,16 @@
         default_variable='portfolio_history',
         union_schema_variable='amazon_ads_union_schemas',
         union_database_variable='amazon_ads_union_databases'
+    )
+}}
+
+{% else %}
+
+{{
+    fivetran_utils.union_connections(
+        connection_dictionary='amazon_ads_sources',
+        single_source_name='amazon_ads',
+        single_table_name='portfolio_history'
     )
 }}
 
